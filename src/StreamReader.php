fe15f22512500ed907049a6daebf5fc8f49e40b9
DarthCorvidus <github@vm01.telton.de>
2025-12-19 23:21:17 +0100

    refactor: removed class constants, had been replaced by Enum

--- src/StreamReader.php
+++ src/StreamReader.php
@@ -1,9 +1,6 @@
 <?php
 namespace plibv4\streams;
 interface StreamReader extends Stream {
-	const SEEK_CUR = SEEK_CUR;
-	const SEEK_SET = SEEK_SET;
-	const SEEK_END = SEEK_END;
 	function read(int $length): string;
 	function seek(int $offset, Seek $whence = Seek::CUR): void;
 	function rewind(): void;
